f63fd2f377d69e9d4a640a37abd78d7f6f6d2df8
hillngeorge <155601421+hillngeorge@users.noreply.github.com>
2024-01-28 18:36:47 -0600

    Add files via upload

---
+++ Sdgku projects/javascript-temperature/script.js
@@ -0,0 +1,23 @@
+console.log("script");
+
+var userName="george";
+
+
+userName="james";
+
+console.log(userName);
+var employeeName="Kory";
+var employeeAge= 27;
+var employee="USA";
+var employee="55589076";
+var employeeSalaryMonthly=20000;
+var employeeSalaryYearly= employeeSalaryMonthly*12;
+
+document.write(
+    `
+
+<h1>Employee Info</h1>
+<p>Name: ${employeeName}</p>
+<p>Age: ${employeeAge}</p>
+`
+);
